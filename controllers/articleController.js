47c4228bb29c5a0a34d4c7da31513ae1c76a1223
Juan Gualdron <juandavidgualdronnavarro@gmail.com>
2022-12-30 17:22:03 -0500

    Added route and controller of all articles and one article

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -43,7 +43,7 @@ const create = (req, res) => {
   }
 
   //Crear el objeto a guardar
-  const article = new Article(req.body)//pasar parametros automaticamente
+  const article = new Article(parameters)//pasar parametros automaticamente
 
   //Asignar valores a objeto basado en el modelo (manual o automatico)
   //article.title = parameters.title forma manual
@@ -67,6 +67,62 @@ const create = (req, res) => {
   })
 }
 
+const getArticles = (req, res) => {
+
+  const consult = Article.find({});
+
+  if(req.params.latest === "latest"){
+    consult.limit(2)
+  }else if(req.params.latest && req.params.latest !== "latest"){
+    return res.status(404).json({
+      status: "error",
+      mensaje: "Parametro ingresado incorrecto"
+    })
+  }
+  
+  consult.sort({date: -1})
+         .exec((error, filteredArticles) => {
+
+    if(error || !filteredArticles){
+      return res.status(404).json({
+        status: "error",
+        mensaje: "No se han encontrado articulos"
+      })
+    }
+
+    return res.status(200).json({
+      status: "success",
+      totalArticles: filteredArticles.length,
+      articles: filteredArticles
+    })
+
+  })
+
+}
+
+const getOneArticle = (req, res) => {
+  //Recoger un id por la url
+  const id = req.params.id
+
+  //Buscar el articulo
+  Article.findById(id, (error, article) => {
+    //Si no existe devolver error
+    if(error || !article){
+      return res.status(404).json({
+        status: "error",
+        mensaje: "No se ha encontrado el articulo"
+      })
+    }
+
+    //Devolver resultado
+    return res.status(200).json({
+      status: "success",
+      article
+    })
+
+  })
+}
+
 module.exports = {
-  test, curso, create
+  test, curso, create, getArticles, getOneArticle
 }
--- routes/articleRoute.js
+++ routes/articleRoute.js
@@ -8,6 +8,8 @@ router.get("/ruta-de-prueba", ArticuloController.test)
 router.get("/curso", ArticuloController.curso)
 
 //Ruta util
+router.get("/articles/:latest?", ArticuloController.getArticles)
+router.get("/article/:id", ArticuloController.getOneArticle)
 router.post("/create", ArticuloController.create)
 
 module.exports = router
